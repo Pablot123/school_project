2b20443ee6cc8c6faba075f5205e14a50dc7856b
Pablot123 <pablotamayo44@gmail.com>
2022-06-21 17:57:30 -0500

    creating classes

---
+++ Estudiante.py
@@ -0,0 +1,33 @@
+from turtle import settiltangle
+from Persona import Persona
+
+class Estudiante(Persona):
+    def __init__(self, nombre, codigo, id, grado, materias, notas) -> None:
+        super().__init__(nombre, codigo, id)
+        self._grado = grado
+        self._materias = materias
+        self._notas = notas
+
+    @property
+    def grado(self):
+        return self._grado
+    @grado.setter
+    def grado(self, grado):
+        self._grado = grado
+    
+    @property
+    def materias(self):
+        return self._materias
+    @materias.setter
+    def materias(self, materias):
+        self._materias = materias
+    
+    @property
+    def notas(self):
+        return self._notas
+    @notas.setter
+    def notas(self, notas):
+        self._notas = notas
+    
+    def promedio_notas():
+        pass
---
+++ Profesor.py
@@ -0,0 +1,22 @@
+from Persona import Persona
+
+class Profesro(Persona):
+
+    def __init__(self, nombre, codigo, id, materia, salario) -> None:
+        super().__init__(nombre, codigo, id)
+        self._materia = materia
+        self._salario = salario
+
+    @property
+    def materia(self):
+        return self._materia
+    @materia.setter
+    def materia(self, materia):
+        self._materia = materia
+    
+    @property
+    def salario(self):
+        return self._salario
+    @salario.setter
+    def salario(self, salario):
+        self._salario = salario
